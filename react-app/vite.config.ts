921b7f96d66b79191d952046566a3fc600e44e90
JS <dev@voidst.one>
2025-05-19 11:15:48 +0000

    Setup shadcn ui and add button

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,11 +1,17 @@
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   server: {
     open: true,
