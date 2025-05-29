f6f7dc26e1578abba22f707b16de14b61b1a55e1
JS <dev@voidst.one>
2025-05-29 08:40:02 +0000

    Add vite-plugin-wasm and vite-plugin-top-level-await

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,12 +1,14 @@
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
 import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
