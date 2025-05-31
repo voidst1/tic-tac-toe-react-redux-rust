4272925a7aaeba772d5f3dc35b5ee2b41596cb9c
JS <dev@voidst.one>
2025-05-31 19:44:31 +0000

    Remove unused files

--- react-app/src/app/store.ts
+++ react-app/src/app/store.ts
@@ -1,13 +1,11 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 import { ticTacToeSlice } from "@/features/tic-tac-toe/ticTacToeSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, quotesApiSlice, ticTacToeSlice)
+const rootReducer = combineSlices(ticTacToeSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
@@ -19,7 +17,7 @@ export const makeStore = (preloadedState?: Partial<RootState>) => {
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+      return getDefaultMiddleware()
     },
     preloadedState,
   })
